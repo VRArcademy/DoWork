c0113a09d941c54aa5a77a82143566fca8514a89
E2ioSin9 <33346779+E2ioSin9@users.noreply.github.com>
2017-12-07 20:47:02 +0800

    DoWork

---
+++ DoWork/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public List<GameObject> weaponList;
+	public GameObject randWeapon;
+	int randNum;
+
+	public enum GameState{WaitToStart, GameStarted, GameEnd}
+	public GameState state;
+
+	void Start () {
+		instance = this;
+		state = GameState.WaitToStart;
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			RandomSpawnWeapon ();
+		}
+		
+	}
+
+	void RandomSpawnWeapon(){
+		randNum = Random.Range (0, 3);
+		print ("Random Num: " + randNum);
+		randWeapon = weaponList [randNum];
+	}
+}
---
+++ DoWork/Assets/Scripts/PlayerControlScripts.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControlScripts : MonoBehaviour {
+
+	Slider PowerBar;
+	int Maxpower = 200;
+	float power = 0f;
+	float PowerChange = 6.0f;
+	public GameObject Weapon;
+	public Transform throwPoint;
+
+
+	void Start () {
+		PowerBar = gameObject.GetComponentInChildren<Slider> ();
+		PowerBarDisActive ();
+	}
+		
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.Space)) {
+			PowerBar.gameObject.SetActive (true);
+			power += PowerChange;
+			if (power < 0 || power > 200) {
+				PowerChange = -PowerChange;
+			}
+			PowerBar.value = power / Maxpower;
+
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			Throw (power);
+			print (power);
+			power = 0;
+			Invoke ("PowerBarDisActive", 2);
+		} 
+	}
+
+	void Throw(float powerValue){
+		Weapon = GameManager.instance.randWeapon;
+		GameObject obj = Instantiate (Weapon, throwPoint.position, Weapon.transform.rotation);
+		Rigidbody2D rdbd = obj.GetComponent<Rigidbody2D> ();
+
+		rdbd.velocity = new Vector2 (0.1f*powerValue, 0.1f*powerValue);
+	}
+
+	void PowerBarDisActive(){
+		PowerBar.gameObject.SetActive (false);
+	}
+}
